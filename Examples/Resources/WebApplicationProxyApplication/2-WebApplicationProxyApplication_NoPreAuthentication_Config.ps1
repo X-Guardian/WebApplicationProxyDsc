a126338af74f004505417cec974a32de69bdc2ab
Simon Heather <simon.heather@zoyoglobal.com>
2019-10-21 11:30:30 +0100

    Updated examples

--- Examples/Resources/WebApplicationProxyApplication/2-WebApplicationProxyApplication_NoPreAuthentication_Config.ps1
+++ Examples/Resources/WebApplicationProxyApplication/2-WebApplicationProxyApplication_NoPreAuthentication_Config.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 .VERSION 1.0.0
-.GUID 20398705-b6b9-4f14-8402-b85835c82567
+.GUID b349af9f-450e-4357-bc62-74c02fd694d8
 .AUTHOR Microsoft Corporation
 .COMPANYNAME Microsoft Corporation
 .COPYRIGHT (c) Microsoft Corporation. All rights reserved.
@@ -19,11 +19,12 @@
 
 <#
     .DESCRIPTION
-        This configuration will publish a web application that specifies the value of AD FS for the
-        ExternalPreauthentication parameter.
+        This configuration will publish a web application named ContosoApp. The resource specifies
+        a backend server URL and an external URL. The application uses pass-through
+        preauthentication.
 #>
 
-Configuration 'WebApplicationProxyApplication_Config'
+Configuration 'WebApplicationProxyApplication_NoPreAuthentication_Config'
 {
     Import-DscResource -ModuleName 'WebApplicationProxyDsc'
 
@@ -34,9 +35,8 @@ Configuration 'WebApplicationProxyApplication_Config'
             Name                          = 'Contoso App'
             ExternalUrl                   = 'https://contosoapp.contoso.com'
             BackendServerUrl              = 'http://contosoapp:8080/'
-            ADFSRelyingPartyName          = 'ContosoAppRP'
-            ExternalCertificateThumbprint = '69DF0AB8434060DC869D37BBAEF770ED5DD0C32A'
-            ExternalPreauthentication     = 'ADFS'
+            ExternalCertificateThumbprint = 'D1A657E1A4F276FCC45613C0F6B3BC91AFC4633F'
+            ExternalPreauthentication     = 'PassThrough'
         }
     }
 }
