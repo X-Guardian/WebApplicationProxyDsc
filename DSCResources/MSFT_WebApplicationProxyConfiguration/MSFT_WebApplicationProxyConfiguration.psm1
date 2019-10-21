3441668b5e83d330ede96a641d913663c2232c9e
Simon Heather <simon.heather@zoyoglobal.com>
2019-10-21 11:29:43 +0100

    Update help files

--- DSCResources/MSFT_WebApplicationProxyConfiguration/MSFT_WebApplicationProxyConfiguration.psm1
+++ DSCResources/MSFT_WebApplicationProxyConfiguration/MSFT_WebApplicationProxyConfiguration.psm1
@@ -1,8 +1,57 @@
 <#
     .SYNOPSIS
-        DSC module for the Web Application Proxy Configuration resource
+        DSC module for the WebApplicationProxyConfiguration resource
 
     .DESCRIPTION
+        The WebApplicationProxyConfiguration DSC resource manages the configuration settings of a Web Application Proxy
+        server. The settings include the Active Directory Federation Services (AD FS) URL, the token signing
+        certificate, and the edge server URI.
+
+    .PARAMETER FederationServiceName
+        Key - String
+        Specifies the name of a Federation Service. This is the Federation Service for which Web Application Proxy
+        provides AD FS proxy functionality and stores the configuration of the Federation Service.
+
+    .PARAMETER ADFSSignOutURL
+        Write - String
+        Specifies the sign out URL for Web Application Proxy.
+
+    .PARAMETER ADFSTokenAcceptanceDurationSec
+        Write - UInt32
+
+    .PARAMETER ADFSTokenSigningCertificatePublicKey
+        Write - String
+        Specifies the thumbprint of the certificate that the federation server uses to sign the edge token. Specify
+        this parameter only when the AD FS token signing certificate changes.
+
+    .PARAMETER ADFSUrl
+        Write - String
+        Specifies the URL for the federation server that is used by the Web Application Proxy.
+
+    .PARAMETER ADFSWebApplicationProxyRelyingPartyUri
+        Write - String
+        Specifies the URI for the Web Application Proxy server.
+
+    .PARAMETER ConfigurationChangesPollingIntervalSec
+        Write - UInt32
+        Specifies the time interval, in seconds, that elapses before the Web Application Proxy servers query a
+        federation server for configuration changes.
+
+    .PARAMETER ConnectedServersName
+        Write - String
+        Specifies an array of Web Application Proxy servers that are connected to a federation server.
+
+    .PARAMETER OAuthAuthenticationURL
+        Write - String
+        Specifies the URL of the federation server that performs Open Authorization (OAuth) authentication when end
+        users connect to a published web application using a Windows Store app.
+
+    .PARAMETER UserIdleTimeoutAction
+        Write - String
+        Allowed values: Signout, Reauthenticate
+
+    .PARAMETER UserIdleTimeoutSec
+        Write - UInt32
 #>
 
 Set-StrictMode -Version 2.0
